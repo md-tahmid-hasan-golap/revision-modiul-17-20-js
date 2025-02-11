00f72a226333881caf90e6b478a182f3095feca1
golap-raj <golapraj47@gmail.com>
2025-02-11 09:12:08 -0600

    resolve

---
+++ for-of-loop.js
@@ -0,0 +1,15 @@
+const numbers = [10, 20, 30, 66, 99, 76, 44];
+console.log(numbers);
+// for(let number of numbers){
+//     console.log(number)
+// }
+
+let i = 0;
+while(i < numbers.length){
+    console.log(numbers[i])
+    i++;
+}
+
+for(let i = 0; i < numbers.length; i++){
+    console.log(numbers[i])
+}
---
+++ while-loop.js
@@ -0,0 +1,73 @@
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i = i + 2
+// }
+// let i = 2;
+// let sum = 0;
+// while(i <= 10){
+//     console.log(i);
+//     sum = sum + i;
+//     console.log("sum number :", sum)
+//     i = i + 2
+// }
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 10){
+//     if(i % 2 === 1){
+//         sum = sum + i;
+//         console.log(i)
+       
+//     }
+//     i++
+// }
+
+// let sum = 0;
+// for(let i = 0; i <=12; i++){
+//     console.log(i)
+//     sum = sum + i;
+//     console.log(sum);
+//     i++
+// }
+// let sum = 0;
+
+// for(let i = 0; i <= 10; i++){
+//     if(i % 2 === 1){
+//         sum = sum + i;
+//         console.log(sum)
+//     }
+// }
+// let sum = 0;
+
+// for(let i = 1; i <= 10; i++){
+//     // console.log(i);
+//     if(i % 2 === 1){
+       
+//         console.log(i)
+//         sum = sum + i;
+//         console.log(sum)
+//     }
+// }
+
+
+// let i = 60;
+// while(i >= 50){
+//     console.log(i)
+//     i--;
+// }
+
+// for(let i = 60; i >= 50; i-- ){
+//     console.log(i)
+//     if(i < 55){
+//         break
+//     }
+// }
+let sum = 0;
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+    if(i % 2 === 0){
+        sum = sum + i;
+    console.log(sum)
+    }
+}
