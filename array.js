3b25806aa58f30be03ad46ca0405b8dcdfc203f0
golap-raj <golapraj47@gmail.com>
2025-02-11 06:05:18 -0600

    array

---
+++ array.js
@@ -0,0 +1,34 @@
+// // number variable array diclaration
+
+// var numbers = [12, 55, 88, 99, 44, 33];
+// console.log(numbers);
+
+// // string variable array diclaration
+
+// var friends = ["Tahmid", "Hasan", "Golap", "Raj"];
+// console.log(friends)
+
+// // boolean variable array diclaration
+
+// var isPased = [true, false]
+// console.log(typeof isPased)
+
+// mixed variable array 
+
+var mixedArray = [12, 55, "golap", "abdullah", true];
+// console.log(mixedArray)
+
+
+
+
+// for(let i = n.length - 1; i > 0; n-- ){
+//     console.log(n[i])
+// }
+// console.log(n)
+// // for(let num of n){
+// //     console.log(num)
+// // }
+// console.log(n.reverse())
+
+ 
+}
---
+++ prestice-task-2.js
@@ -0,0 +1,15 @@
+// task number - 1
+
+var drinksPrice = 500;
+if(drinksPrice >= 500){
+    // 3 % discount
+    var discount = drinksPrice * 3 / 100;
+    var payout = drinksPrice - discount;
+    console.log(payout);
+    console.log(discount)
+}
+else if(drinksPrice < 400){
+    // 3 % jorimana
+    var jorimana = drinksPrice + 30;
+    var payout = drinksPrice + jorimana
+}
