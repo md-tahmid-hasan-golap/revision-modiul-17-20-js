292c372394c5b44698b21c4a19330f6be58c3abb
golap-raj <golapraj47@gmail.com>
2025-02-11 05:39:34 -0600

    ternary

---
+++ ternary-if-else.js
@@ -0,0 +1,12 @@
+var i = 20;
+// if(i > 10){
+//     console.log("thika ache")
+// }
+// else{
+//     console.log("thik nai")
+// }
+
+// ternary  oparator a likha
+var price = i >= 20 ? "bhai re bhai" : "go vai go"
+console.log(price)
+
