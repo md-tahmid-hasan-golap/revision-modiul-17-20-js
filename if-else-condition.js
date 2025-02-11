dc65503a758da208d191429023f47dc7c99dc1ac
golap-raj <golapraj47@gmail.com>
2025-02-11 05:02:35 -0600

    if else

---
+++ if-else-condition.js
@@ -0,0 +1,19 @@
+// var age = 12;
+// if(age > 10){
+//     console.log("Upni malai Dhukte parben")
+// }
+// else{
+//     console.logo("Upni malai Dhukte parben nha")
+// }
+
+
+var age = 15;
+if(age >= 18){
+    console.log("Upni malai jete parben")
+}
+else if(age >= 13){
+    console.log("upni malar kichu onso dekte parben")
+}
+else{
+    console.log("upni bari fire jan")
+}
