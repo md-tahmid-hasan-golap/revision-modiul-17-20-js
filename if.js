c175c400dac3b72b22dee5686eed4ce50eb722bb
golap-raj <golapraj47@gmail.com>
2025-02-11 05:12:06 -0600

    if

---
+++ if.js
@@ -0,0 +1,20 @@
+// var biriyaniPrice = 150;
+// if(biriyaniPrice < 200){
+//     console.log("taile mama biriyani khabo")
+// }
+
+var wight = 55;
+if(wight < 20){
+    console.log("ami nije nije niye asbo")
+}
+else{
+    console.log("ami riksha te kore niye asbo")
+}
+
+var price = 20;
+if(price <= 20){
+    console.log("oky")
+}
+else{
+    console.log("not oky")
+}
