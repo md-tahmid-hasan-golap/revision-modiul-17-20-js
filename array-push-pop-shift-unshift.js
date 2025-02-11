8809a32b260852008c5a70568029b62a240144af
golap-raj <golapraj47@gmail.com>
2025-02-11 06:17:22 -0600

    add push

---
+++ array-push-pop-shift-unshift.js
@@ -0,0 +1,14 @@
+var numbers = [100, 200, 300, 400, 500];
+console.log(numbers)
+numbers.push(10);
+console.log(numbers)
+numbers.pop();
+numbers.pop()
+console.log(numbers)
+
+numbers.unshift(1)
+console.log(numbers)
+numbers.shift()
+numbers.shift()
+numbers.shift()
+ console.log(numbers)
