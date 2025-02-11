fa57c836d5cec797744b895a9c9e4bf7f31a83d2
golap-raj <golapraj47@gmail.com>
2025-02-11 05:35:06 -0600

    add optional

---
+++ optional.js
@@ -0,0 +1,17 @@
+var price = 100;
+if(price > 200){
+    console.log("Oh nice tho")
+}
+else{
+    if(price > 150){
+        console.log("bha")
+    }
+    else{
+        if(price > 80){
+            console.log("aso aso")
+        }
+        else{
+            console.log("jaa vaag")
+        }
+    }
+}
