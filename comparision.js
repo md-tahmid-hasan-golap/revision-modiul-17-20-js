b4241de8684e5fb56a19b5cfd12e2aabdd2825e2
golap-raj <golapraj47@gmail.com>
2025-02-11 04:53:18 -0600

    resolve

---
+++ comparision.js
@@ -0,0 +1,11 @@
+console.log(10 ==  10);
+console.log(10 > 10);
+console.log(10 >= 10);
+
+console.log(100 < 10);
+
+var applePrice = 50;
+var orangePrice = 50;
+console.log(applePrice >= orangePrice);
+console.log( 10 == "10");
+console.log(10 === " 10")
