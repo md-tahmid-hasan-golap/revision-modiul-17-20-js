ca55d13259ac29a4ba6b24db6de7e85bdacfd6e3
golap-raj <golapraj47@gmail.com>
2025-02-11 05:21:16 -0600

    if else

---
+++ multiple-if-else.js
@@ -0,0 +1,25 @@
+const salary = 30000;
+const isBcs = true;
+const wight = 55;
+
+if(salary > 25000 && wight > 60){
+    console.log("aso baba bose")
+}
+else if (salary > 25000 || wight > 40){
+    console.log("hmm baba cholbe tho")
+}
+else{
+    console.log("baba tumi akkhoni jou")
+}
+// if(salary > 20000 && wight > 70){
+//     console.log("aso kobul")
+// }
+// else{
+//     console.log("jou vago")
+// }
+// if(salary > 20000){
+//     console.log("ASo baba kobul")
+// }
+// else{
+//     console.logo("onno patro klhujo")
+// }
