be1993a67013029fd0f7c11b4da2de0839f214e9
golap-raj <golapraj47@gmail.com>
2025-02-11 06:21:53 -0600

    add includs

---
+++ includs.js
@@ -0,0 +1,9 @@
+var names = ["tahmid", "hasan", "golap", "baba"];
+// console.log(names.includes("Hasan"))
+
+if(names.includes("tahmid")){
+    console.log("party hobe")
+}
+else{
+    console.log("no party")
+}
