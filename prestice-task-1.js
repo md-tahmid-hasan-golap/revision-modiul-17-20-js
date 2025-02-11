46bde263e648fcc5b58baf00cbaeb57171c77cb4
golap-raj <golapraj47@gmail.com>
2025-02-11 04:07:44 -0600

    add prestice

---
+++ prestice-task-1.js
@@ -0,0 +1,27 @@
+// task number - 1
+
+var myMoney = 1000;
+var applePrice = 250;
+var orangrPrice = 485;
+var totalPrice = applePrice + orangrPrice;
+console.log(myMoney - totalPrice)
+
+// task number - 2
+var bangla = 75.25;
+var english = 65;
+var math = 80;
+var biology = 35.45
+var figix = 99.50
+var totalMark = bangla + english + math + biology + figix;
+console.log(totalMark.toFixed(2))
+
+// task number - 3
+
+var price = 119;
+price %= 5;
+console.log(price)
+
+// task number - 4
+
+
+
